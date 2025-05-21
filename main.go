3fc48fdbf3cf3517739ba0925cd020caa1a4ed43
GabrielModog <modog.dev@gmail.com>
2025-05-21 04:27:10 -0300

    lazy read csv

--- main.go
+++ main.go
@@ -1,7 +1,25 @@
 package main
 
-import "fmt"
+import (
+	csv "encoding/csv"
+	"fmt"
+	log "log"
+	os "os"
+)
+
+type Question struct {
+	question string
+	anwser   string
+}
 
 func main() {
-  fmt.Println("Lazy Quiz Game")
+	file, err := os.Open("problem.csv")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	reader := csv.NewReader(file)
+	reacords, _ := reader.ReadAll()
+
+	fmt.Println(reacords)
 }
